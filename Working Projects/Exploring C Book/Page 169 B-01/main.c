53907b9ae76289d737f7cc977600cf51aee9219a
Vishal Ranjan <104883829+Mr-Vishal-Ranjan@users.noreply.github.com>
2022-09-29 00:08:52 +0530

    Update main.c

--- Working Projects/Exploring C Book/Page 169 B-01/main.c
+++ Working Projects/Exploring C Book/Page 169 B-01/main.c
@@ -14,37 +14,37 @@ int main(){
             x = num0 + x;
             rem = num % 100;
             num = rem;
-            printf("\nNumber of 100 notes required is %d\n",x);
+            printf("\nNumber of 100 notes required is %d\n",num0);
 
             num0 = num / 50;
             x = num0 + x;
             rem = num % 50;
             num = rem;
-            printf("\nNumber of 50 notes required is %d\n", x);
+            printf("\nNumber of 50 notes required is %d\n", num0);
 
             num0 = num / 10;
             x = num0 + x;
             rem = num % 10;
             num = rem;
-            printf("\nNumber of 10 notes required is %d\n", x);
+            printf("\nNumber of 10 notes required is %d\n", num0);
 
             num0 = num / 5;
             x = num0 + x;
             rem = num % 5;
             num = rem;
-            printf("\nNumber of 5 notes required is %d\n", x);
+            printf("\nNumber of 5 notes required is %d\n", num0);
 
             num0 = num / 2;
             x = num0 + x;
             rem = num % 2;
             num = rem;
-            printf("\nNumber of 2 notes required is %d\n", x);
+            printf("\nNumber of 2 notes required is %d\n", num0);
 
             num0 = num / 1;
             x = num0 + x;
             rem = num % 1;
             num = rem;
-            printf("\nNumber of 1 notes required is %d\n", x);
+            printf("\nNumber of 1 notes required is %d\n", num0);
         }
         
         
