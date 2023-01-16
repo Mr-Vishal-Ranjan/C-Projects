1e53fa9d1a94e8d4b1c8d99246162ca5b73a9b17
Mr-Vishal-Ranjan <vr842002@gmail.com>
2023-01-16 12:21:44 +0530

    ASSIGMENT

---
+++ PPA FILE CODES/EX--5 Funtions/Q_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void hello(){
+
+    printf("Hello World!");
+}
+
+int main()
+{
+    hello();
+    return 0;
+}
---
+++ PPA FILE CODES/EX--5 Funtions/Q_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int multiply(int x , int y)
+{
+    printf("Multiply is %d",x*y);
+}
+
+int main()
+{
+    int a ,b ;
+    printf("ENTER TWO NUMBER -->");
+    scanf("%d %d",&a,&b);
+
+    multiply(a,b);
+
+    return 0;
+}
